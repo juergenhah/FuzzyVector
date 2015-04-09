702ae7bb27c17c83c3241606aad7e76861d1707f
juergenhah <hahn@geoinfo.tuwien.ac.at>
2015-04-09 13:28:15 +0200

    polishing

--- src/FuzzyExamples.hs
+++ src/FuzzyExamples.hs
@@ -1,6 +1,6 @@
 {-|
 Module      : FuzzyExamples
-Description : defines fuzzy vectors
+Description : includes fuzzy vectors used for examples
 Copyright   : (c) Juergen Hahn, 2015
                   Simon Scheider, 2015
 License     : GPL-3
@@ -48,7 +48,7 @@ fuzzySpoonMap space = toFuzzyMap space fuzzySpoon
 
 fuzzyRealSpoon :: CrispVector 
                -> MemberShipValue
-fuzzyRealSpoon = fuzzyUnion' g handle 
+fuzzyRealSpoon = fuzzyUnion g handle 
  where        g = fuzzyCircleTemplate 3 2 (crispVector (-5.0) 0.0 )     
 
 fuzzyRealSpoonMap space = toFuzzyMap space fuzzyRealSpoon
@@ -151,21 +151,13 @@ objB v =min (xTrapez $ getX v) (yTrapez $ getY v)
 
 objBMap space= toFuzzyMap space objB
 
-
-                                                          {-    | x < 3 || x> 4  =0
-                                                               | y < (1)  || y> (2) =0
-                                                               | otherwise = 1
-                                                               where x = V.head v
-                                                                     y = V.head.V.drop 1 $ v
--}
-
--- * objects for the reference frame transforamtions
+-- * objects for the reference frame transformations
 
 largehouse  :: CrispVector 
             -> MemberShipValue
 largehouse v
- | x < (-12) || x> (-2) =0  -- -6 -1
- | y < (6) || y> (15)  =0   -- 6 9
+ | x < (-12) || x> (-2) =0  
+ | y < (6) || y> (15)  =0   
  | otherwise = 1
    where x = getX v
          y = getY v
@@ -186,8 +178,8 @@ largehouseFrontMap space= toFuzzyMap space largehouseFront
 realLargehouseFront  :: CrispVector
                      -> MemberShipValue
 realLargehouseFront v 
- | x < (-9) || x> (-5)  =0  -- -4  -3
- | y < (5) || y> (6) = 0     -- 5   6
+ | x < (-9) || x> (-5)  =0  
+ | y < (5) || y> (6) = 0    
  | otherwise = 1
   where x = getX v
         y = getY v
@@ -220,111 +212,3 @@ north v
  where x = getX v
        y = getY v
 northMap space= toFuzzyMap space north
-
-                                                     {-
-                                                      subFork :: CrispVector 
-                                                              -> MemberShipValue
-                                                      subFork v
-                                                       | x < 0 || x> 1 =0
-                                                       | y < 2 || y> 3 =0
-                                                       | otherwise = 1
-                                                       where x = getX v
-                                                             y = getY v
-
-newFork :: CrispVector 
-        -> MemberShipValue
-newFork v
- | x < 0 || x> 1 =0
- | y < 3 || y> 4 =0
- | otherwise = 1
- where x = getX v
-       y = getY v
-
-
-myFork :: CrispVector 
-       -> MemberShipValue
-myFork v
- | x < 0 || x> 1 =0
- | y < 0 || y> 1 =0
- | otherwise = 1
- where x = getX v
-       y = getY v
-
-toRotFork :: CrispVector 
-          -> MemberShipValue
-toRotFork v
- | x < 0 || x> 1 =0
- | y < 0 || y> 2 =0
- | otherwise = 1
- where x = getX v
-       y = getY v
-       
-
-toRotSpoon :: CrispVector 
-           -> MemberShipValue
-toRotSpoon v
- | x < 0 || x> 2 =0
- | y < 0 || y> 1 =0
- | otherwise = 1
- where x = getX v
-       y = getY v
-       
-normSpoon :: CrispVector
-          -> MemberShipValue
-normSpoon v
- | x < 4 || x> 5 =0
- | y < 0 || y> 1 =0
- | otherwise = 1
- where x = getX v
-       y = getY v
-
-mySpoon :: CrispVector
-        -> MemberShipValue
-mySpoon v
- | x < 2 || x> 3 =0
- | y < 2 || y> 3 =0
- | otherwise = 1
- where x = getX v
-       y = getY v
-
-myPyramide :: CrispVector
-           -> MemberShipValue
-myPyramide v
- | x<0 || y<0 = 0
- | x>=5 || y>=5 = 0
- | (0<= x && x<= 1) && (0<=y && y<= 1)=1
- | otherwise = (-1/4) * sqrt(x*x+y*y) +6/4
-   where x = getX v
-         y = getY v
-
-mySteps :: CrispVector
-        -> MemberShipValue
-mySteps v
- | (x >= (2+xdel) && x<=(3+xdel)) && (y >= (2+ydel) && y<=(3+ydel))=1
- | (x >= (1+xdel) && x<=(4+xdel)) && (y >= (1+ydel) && y<=(4+ydel))=0.66
- | (x >= (0+xdel) && x<=(5+xdel)) && (y >= (0+ydel) && y<=(5+ydel))=0.33
- | otherwise = 0
-    where x = getX v
-          y = getY v
-          xdel= 2
-          ydel= 1
-
-myXAxis :: CrispVector
-        -> MemberShipValue
-myXAxis v
- | x  <3 || x>4 =0
- | y  <0 || y>1 =0
- |  otherwise =1
- where x = getX v
-       y = getY v
-
-myYAxis :: CrispVector 
-        -> MemberShipValue
-myYAxis v
- | x  <0 || x>1 =0
- | y  <7 || y>8 =0
- | otherwise =1
- where x = getX v
-       y = getY v
-
--}
