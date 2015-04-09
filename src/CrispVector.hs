dbb9a23796458d8a95210f509be4e9aa7d171e98
juergenhah <hahn@geoinfo.tuwien.ac.at>
2015-04-09 13:28:04 +0200

    polishing

--- src/CrispVector.hs
+++ src/CrispVector.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
 {-|
 Module      : CrispVector
-Description : abstraction from Data.Vector package and functions for crisp vector
+Description : abstraction from Data.Vector package to create a crisp vector
 Copyright   : (c) Juergen Hahn, 2015
                   Simon Scheider, 2015
 License     : GPL-3
@@ -36,7 +36,6 @@ data CrispVector = C (V.Vector Double) deriving (Eq,Ord,Read,Show)
 instance NFData CrispVector where
  rnf (C v) = rnf v
  
-
 -- | represents an Angle of an vector
 type Angle = Double 
 
@@ -49,18 +48,18 @@ crispVector x y = C (V.fromList [x,y])
 -- | extract X value of the vector
 getX :: CrispVector -- ^ crisp vector with at least 1 dimension
      -> Double      -- ^ first element of the crisp vector
-getX (!C !v) = V.head v    
+getX (C v) = V.head v    
 
 -- | extract Y value of the vector
 getY :: CrispVector -- ^ crisp vector with at least 2 dimensions
      -> Double      -- ^ second element of the crisp vector
-getY (!C !v) = V.head . V.drop 1 $ v   
+getY (C v) = V.head . V.drop 1 $ v   
 
 -- | multiplies each element of the vector
 scaleCrispVector :: Double      -- ^ multiplier for vector
                  -> CrispVector -- ^ vector to be scaled
                  -> CrispVector -- ^ multiplied vector
-scaleCrispVector !lambda (!C !v)  =C (V.map (*lambda) v )
+scaleCrispVector lambda (C v)  =C (V.map (*lambda) v )
 
 -- | creates the sum of the vectors included into the list
 -- for an empty list a 0.0 0.0 vector is returned
@@ -73,13 +72,13 @@ sumVec (v:vs) =add v $ sumVec vs
 add :: CrispVector
     -> CrispVector
     -> CrispVector
-add (!C a ) (!C b)  = C (a+b)
+add (C a ) (C b)  = C (a+b)
 
 -- | subtracts the second vector from the first
 sub :: CrispVector
      -> CrispVector
      -> CrispVector
-sub (!C a ) (!C b)  = C (a-b)
+sub (C a ) (C b)  = C (a-b)
 
 -- | calculates the euclidian distance of two points indicated by two crisp vectors
 distance :: CrispVector -- ^ first point - vector
